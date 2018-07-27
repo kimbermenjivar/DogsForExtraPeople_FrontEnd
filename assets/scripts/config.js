ad418994f873ca26e5296f4d0ea1266d49eb24d3
kimbermenjivar <kimbermenjivar@gmail.com>
2018-07-27 08:58:30 -0400

    updated heroku url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://sheltered-hollows-32763.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
